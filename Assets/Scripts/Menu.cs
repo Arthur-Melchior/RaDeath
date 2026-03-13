dc16ca419c40c2760933d2608df693bd440a6a06
Noah <noah.sonntag@gmail.com>
2026-03-13 19:51:41 +0100

    MainMenu

---
+++ Assets/Scripts/Menu.cs
@@ -0,0 +1,24 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Menu : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public void OnClickPlay()
+    {
+        Debug.Log("METAL!!!");
+        SceneManager.LoadScene("SampleScene");
+    }
+    public void OnClickExit()
+    {
+        #if UNITY_EDITOR
+            UnityEditor.EditorApplication.ExitPlaymode();
+        #else
+            Application.Quit();
+        #endif
+            Debug.Log("ByeBye");
+        
+       
+        
+    }
+}
