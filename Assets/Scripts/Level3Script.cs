ae9c933ee9281a5faabe202ad06ac7cb4f39ef63
Arthur-Melchior <108128616+Arthur-Melchior@users.noreply.github.com>
2026-03-15 09:30:20 +0100

    added 3rd level

---
+++ Assets/Scripts/Level3Script.cs
@@ -0,0 +1,51 @@
+﻿using UnityEngine;
+using UnityEngine.Events;
+using UnityEngine.InputSystem;
+using UnityEngine.UI;
+
+public class Level3Script : MonoBehaviour
+{
+    [SerializeField] private InputScript inputScript;
+    [SerializeField] private GameObject rightArm;
+    [SerializeField] private Image leftFillImage;
+    [SerializeField] private Image rightFillImage;
+    public UnityEvent onWin;
+
+    private bool _mouseDownWin;
+    private bool _mouseUpWin;
+
+    public void OnMouseMove(InputAction.CallbackContext ctx)
+    {
+        if (inputScript.mouseScore > 1 && !_mouseDownWin)
+        {
+            rightFillImage.fillAmount = inputScript.mouseScore / inputScript.mouseMoveScore;
+        }
+        if (inputScript.mouseScore < -1 && !_mouseUpWin)
+        {
+            leftFillImage.fillAmount = -inputScript.mouseScore / inputScript.mouseMoveScore;
+        }
+
+        rightArm.transform.rotation = Quaternion.Euler(0, 0, inputScript.mouseScore * 2);
+    }
+
+    public void OnWin()
+    {
+        if (inputScript.mouseScore > 1)
+        {
+            _mouseDownWin = true;
+            inputScript.inputType = InputType.MouseUp;
+            inputScript.mouseScore = 0;
+        }
+        if (inputScript.mouseScore < -1)
+        {
+            _mouseUpWin = true;
+            inputScript.inputType = InputType.MouseDown;
+            inputScript.mouseScore = 0;
+        }
+
+        if (_mouseUpWin && _mouseDownWin)
+        {
+            onWin?.Invoke();
+        }
+    }
+}
