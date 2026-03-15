88aaaf255f1236664a813ff756f4e94c9c6366a2
Arthur-Melchior <108128616+Arthur-Melchior@users.noreply.github.com>
2026-03-15 11:52:56 +0100

    added level 4

---
+++ Assets/Scripts/Level4Script.cs
@@ -0,0 +1,45 @@
+using System;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class Level4Script : MonoBehaviour
+{
+    public float requiredSuccess;
+    public float maxFailure;
+    public UnityEvent onWin;
+    public UnityEvent onFail;
+    private float _successCount;
+    private float _failureCount;
+    
+    public void OnSuccess(GameObject go)
+    {
+        _successCount++;
+        Destroy(go);
+        if (_successCount > requiredSuccess)
+        {
+            onWin?.Invoke();
+        }
+    }
+
+    public void OnFail(GameObject go)
+    {
+        _failureCount++;
+        Destroy(go);
+        if (_failureCount > maxFailure)
+        {
+            onFail?.Invoke();
+        }
+    }
+    
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        var script = other.GetComponent<ArrowScript>();
+        script.isCorrect = true;
+    }
+    
+    private void OnTriggerExit2D(Collider2D other)
+    {
+        var script = other.GetComponent<ArrowScript>();
+        script.isCorrect = false;
+    }
+}
